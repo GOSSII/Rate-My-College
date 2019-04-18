03241140d8ca1cc03522131e86fcbd6b10ba82f5
“GOSSII” <dipeshpuri9@gmail.com>
2019-04-18 16:23:21 -0400

    Get Active Survey Integration

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+// import { ContactListService } from 'src/app/services/contact-list.service';
+import {  SurveyService } from '../services/survey.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
+
+// import { Contact } from 'src/app/models/contact';
+import { Contact } from '../models/contact';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +13,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  contacts: Contact[];
 
-  constructor() { }
+  constructor(
+    private surveyService: SurveyService,
+    private flashMessage: FlashMessagesService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.contacts = new Array<Contact>();
+
+    this.displayContactList();
+  }
+
+
+  displayContactList(): void {
+    this.surveyService.getActiveSurvey().subscribe(data => {
+      console.log("ds-> ", data);
+      if(data.success) {
+        this.contacts = data.SurveyList;
+      } else {
+        this.flashMessage.show('User must be logged-in', {cssClass: 'alert-danger', timeOut: 3000});
+      }
+    });
   }
 
 }
---
+++ client/src/app/services/survey.service.ts
@@ -0,0 +1,50 @@
+/* 
+Project: Career-Portfolio-MEAN-STACK    
+Name: Dipeshpuri Goswami
+Id: 300984229 
+Date 29, March 2019 
+*/
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Contact } from '../models/contact';
+import { User } from '../models/user';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SurveyService {
+  private user: User;
+
+
+   private endpoint = 'http://localhost:3000/api/takesurvey/';
+  // private endpoint = 'https://career-portfolio-mean-stack.herokuapp.com/api/todo/';
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+      'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
+    })
+  };
+
+  constructor(private http: HttpClient) {
+    this.user = new User();
+  }
+
+  public getActiveSurvey(): Observable<any> {
+    return this.http.get<any>(this.endpoint, this.httpOptions);
+  }
+
+ 
+  // public addContact(contact: Contact): Observable<any> {
+    
+  //   return this.http.post<any>(this.endpoint + 'add', contact, this.httpOptions);
+  // }
+
+  
+}
